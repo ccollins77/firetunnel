bace008dd4de4cd8ae283ac3850820cf520b843e
netblue30 <netblue30@yahoo.com>
2018-08-23 21:45:45 -0400

    make test-travis

---
+++ test/firetunnel/firetunnel-travis.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# This file is part of Firetunnel project
+# Copyright (C) 2018 Firetunnel Authors
+# License GPL v2
+
+echo "abcdefg" > /etc/firetunnel/firetunnel.secret
+
+./firetunnel.sh
